1251ff48296d6d69bc5c42bcbde886f25bd6a893
quyennguyenvu <leo.quyennguyen@gmail.com>
2021-03-21 14:23:58 +0700

    complete dickhead's website

--- src/index.js
+++ src/index.js
@@ -4,18 +4,17 @@ import ReactDOM from 'react-dom'
 import MyImage from './assets/images/background.jpg'
 import './index.css'
 import { Row, Col } from 'react-bootstrap'
-// import Col from 'react-bootstrap/Col'
 
 const App = () => {
   return (
     <div className="wrapper d-flex justify-content-center">
       <div className="content">
         <img src={MyImage} />
-        <Row className="infobox">
-          <Col className="dickhead">
+        <div className="infobox">
+          <div className="dickhead">
             <h1>RA</h1>
-          </Col>
-          <Col className="dickhead-contacts text-right">
+          </div>
+          <div className="dickhead-contacts text-right">
             <h2>rigor atelier</h2>
             <div className="contact">
               <a href="tel:+84395799544">+84.39 5799 544</a>
@@ -23,9 +22,16 @@ const App = () => {
             <div className="contact">
               <a href="mailto:rigoratelier@gmail.com">rigoratelier@gmail.com</a>
             </div>
-            <div className="contact">70 Phan Dinh Phung, Ha Noi</div>
-          </Col>
-        </Row>
+            <div className="contact">
+              <a href="facebook.com/rigoratelier">facebook.com/rigoratelier</a>
+            </div>
+            <div className="contact">
+              <a href="https://goo.gl/maps/wWdSX4vubEqTMMWm8" target="_blank">
+                70 Phan Dinh Phung, Ha Noi
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
