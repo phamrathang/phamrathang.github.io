addec00c5783e94f7cda4e5abf02b2c8cbe4980c
quyennguyenvu <leo.quyennguyen@gmail.com>
2026-02-20 22:17:08 +0700

    feat: upgrade to hashRouter

--- src/routes.js
+++ src/routes.js
@@ -24,25 +24,3 @@ const Router = () => (
 )
 
 export default Router
-
-// import React from 'react'
-
-// import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
-
-// import Home from './pages/home'
-// import Resume from './pages/resume'
-
-// function Router() {
-//   return (
-//     <HashRouter>
-//       <Routes>
-//         <Route exact path="/" element={<Home />} />
-//         <Route exact path="/resume" element={<Resume />} />
-//         {/* Not Found */}
-//         <Route element={() => <Navigate to="/" />} />
-//       </Routes>
-//     </HashRouter>
-//   )
-// }
-
-// export default Router
